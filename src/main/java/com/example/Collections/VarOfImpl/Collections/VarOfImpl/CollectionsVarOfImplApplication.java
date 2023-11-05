b113f290f6c6c94e7014cca063649ecb26880e1d
Roman <roOmkin@gmail.com>
2023-11-05 19:35:57 +0700

    Мастер ветка - Коллекции: многообразие реализаций.

---
+++ src/main/java/com/example/Collections/VarOfImpl/Collections/VarOfImpl/CollectionsVarOfImplApplication.java
@@ -0,0 +1,13 @@
+package com.example.Collections.VarOfImpl.Collections.VarOfImpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionsVarOfImplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionsVarOfImplApplication.class, args);
+	}
+
+}
